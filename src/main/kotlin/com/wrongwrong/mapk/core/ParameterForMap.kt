b56b1c0d7e51bc3d5f0f1619eb8c2a173ea2a445
wrongwrong <botanri1995@gmail.com>
2020-01-25 15:23:58 +0900

    コンストラクタとcompanion objectとで扱いを変える必要が有ったため修正

--- src/main/kotlin/com/wrongwrong/mapk/core/ParameterForMap.kt
+++ src/main/kotlin/com/wrongwrong/mapk/core/ParameterForMap.kt
@@ -2,19 +2,47 @@ package com.wrongwrong.mapk.core
 
 import com.wrongwrong.mapk.annotations.SingleArgCreator
 import kotlin.reflect.KClass
-import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
-import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.companionObjectInstance
 import kotlin.reflect.full.functions
 
 class ParameterForMap(val param: KParameter, propertyNameConverter: (String) -> String) {
     val clazz: KClass<*> = (param.type.classifier as KClass<*>)
     val name: String = propertyNameConverter(param.name!!)
 
-    val creatorMap: Map<KClass<*>, KFunction<*>>? by lazy {
-        listOfNotNull(clazz.constructors, clazz.companionObject?.functions)
-            .flatten()
-            .filter { it.annotations.any { annotation -> annotation is SingleArgCreator } }
-            .associateBy { (it.parameters.single().type.classifier as KClass<*>) }
+    val creatorMap: Map<KClass<*>, (Any) -> Any?> by lazy {
+        creatorsFromConstructors(clazz) + creatorsFromCompanionObject(clazz)
     }
+}
+
+private fun creatorsFromConstructors(clazz: KClass<*>): Map<KClass<*>, (Any) -> Any?> {
+    return clazz.constructors
+        .filter { it.annotations.any { annotation -> annotation is SingleArgCreator } }
+        .associate { func ->
+            val call = { it: Any ->
+                func.call(it)
+            }
+
+            (func.parameters.single { param -> param.kind == KParameter.Kind.VALUE }.type.classifier as KClass<*>) to
+                    call
+        }
+}
+
+private fun creatorsFromCompanionObject(clazz: KClass<*>): Map<KClass<*>, (Any) -> Any?> {
+    return clazz.companionObjectInstance?.let { companionObject ->
+        companionObject::class.functions
+            .filter { it.annotations.any { annotation -> annotation is SingleArgCreator } }
+            .associate { function ->
+                val params = function.parameters
+                if (params.size != 2) {
+                    throw IllegalArgumentException("This function is not compatible num of arguments.")
+                }
+
+                val func = { value: Any ->
+                    function.call(companionObject, value)
+                }
+
+                (params.single { param -> param.kind == KParameter.Kind.VALUE }.type.classifier as KClass<*>) to func
+            }
+    }?: emptyMap()
 }
