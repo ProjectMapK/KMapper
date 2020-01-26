c1d4b6c355a7751452013c6342ca08646d4926a1
wrongwrong <botanri1995@gmail.com>
2020-01-26 15:23:09 +0900

    ゲッターにもエイリアスを設定できるよう修正

--- src/main/kotlin/com/wrongwrong/mapk/annotations/PropertyAlias.kt
+++ src/main/kotlin/com/wrongwrong/mapk/annotations/PropertyAlias.kt
@@ -1,5 +1,5 @@
 package com.wrongwrong.mapk.annotations
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.PROPERTY_GETTER)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class PropertyAlias(val value: String)
