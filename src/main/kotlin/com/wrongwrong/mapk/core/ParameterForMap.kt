49af7c8a8e69f06d661ea1e5969048f936240454
wrongwrong <boranti1995@gmail.com>
2020-02-03 20:15:53 +0900

    subclassだった場合に正常にmapできない問題を修正

--- src/main/kotlin/com/wrongwrong/mapk/core/ParameterForMap.kt
+++ src/main/kotlin/com/wrongwrong/mapk/core/ParameterForMap.kt
@@ -7,6 +7,7 @@ import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.companionObjectInstance
 import kotlin.reflect.full.functions
+import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.staticFunctions
 
 class ParameterForMap(val param: KParameter, propertyNameConverter: (String) -> String) {
@@ -19,36 +20,41 @@ class ParameterForMap(val param: KParameter, propertyNameConverter: (String) -> 
     val javaClazz: Class<*> by lazy {
         clazz.java
     }
-    val creatorMap: Map<KClass<*>, (Any) -> Any?> by lazy {
+    // リストの長さが小さいと期待されるためこの形で実装しているが、理想的にはmap的なものが使いたい
+    private val creators: Set<Pair<KClass<*>, (Any) -> Any?>> by lazy {
         creatorsFromConstructors(clazz) + creatorsFromStaticMethods(clazz) + creatorsFromCompanionObject(clazz)
     }
+
+    // 引数の型がcreatorに対して入力可能ならcreatorを返す
+    fun getCreator(input: KClass<*>): ((Any) -> Any?)? =
+        creators.find { (key, _) -> input.isSubclassOf(key) }?.let { (_, creator) -> creator }
 }
 
-private fun Collection<KFunction<*>>.getCreatorMapFromFunctions(): Map<KClass<*>, (Any) -> Any?> {
+private fun Collection<KFunction<*>>.getCreatorMapFromFunctions(): Set<Pair<KClass<*>, (Any) -> Any?>> {
     return filter { it.annotations.any { annotation -> annotation is SingleArgCreator } }
-        .associate { func ->
+        .map { func ->
             val call = { it: Any ->
                 func.call(it)
             }
 
             (func.parameters.single { param -> param.kind == KParameter.Kind.VALUE }.type.classifier as KClass<*>) to
                     call
-        }
+        }.toSet()
 }
 
-private fun creatorsFromConstructors(clazz: KClass<*>): Map<KClass<*>, (Any) -> Any?> {
+private fun creatorsFromConstructors(clazz: KClass<*>): Set<Pair<KClass<*>, (Any) -> Any?>> {
     return clazz.constructors.getCreatorMapFromFunctions()
 }
 
-private fun creatorsFromStaticMethods(clazz: KClass<*>): Map<KClass<*>, (Any) -> Any?> {
+private fun creatorsFromStaticMethods(clazz: KClass<*>): Set<Pair<KClass<*>, (Any) -> Any?>> {
     return clazz.staticFunctions.getCreatorMapFromFunctions()
 }
 
-private fun creatorsFromCompanionObject(clazz: KClass<*>): Map<KClass<*>, (Any) -> Any?> {
+private fun creatorsFromCompanionObject(clazz: KClass<*>): Set<Pair<KClass<*>, (Any) -> Any?>> {
     return clazz.companionObjectInstance?.let { companionObject ->
         companionObject::class.functions
             .filter { it.annotations.any { annotation -> annotation is SingleArgCreator } }
-            .associate { function ->
+            .map { function ->
                 val params = function.parameters
                 if (params.size != 2) {
                     throw IllegalArgumentException("This function is not compatible num of arguments.")
@@ -59,6 +65,6 @@ private fun creatorsFromCompanionObject(clazz: KClass<*>): Map<KClass<*>, (Any) 
                 }
 
                 (params.single { param -> param.kind == KParameter.Kind.VALUE }.type.classifier as KClass<*>) to func
-            }
-    }?: emptyMap()
+            }.toSet()
+    }?: emptySet()
 }
