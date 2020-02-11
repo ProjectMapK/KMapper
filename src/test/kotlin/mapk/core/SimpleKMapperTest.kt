b42c5d0a07a897545ef47ce452c6b0bf766b93f1
wrongwrong <boranti1995@gmail.com>
2020-02-11 23:19:52 +0900

    複数ソースからマップするテストを追加

--- src/test/kotlin/mapk/core/SimpleKMapperTest.kt
+++ src/test/kotlin/mapk/core/SimpleKMapperTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 import kotlin.reflect.full.primaryConstructor
 
 data class Dst(
@@ -27,6 +28,8 @@ data class Src1(
         get() = arg1.toByte()
     val arg4 = null
 }
+
+data class Src2(val arg2: String?)
 
 @DisplayName("単純なマッピングのテスト")
 class SimpleKMapperTest {
@@ -115,6 +118,44 @@ class SimpleKMapperTest {
                 assertEquals(it.arg1, 0)
                 assertEquals(it.arg2, null)
                 assertEquals(it.arg3, 0.toByte())
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("複数ソースからのマップ")
+    inner class FromMultipleSrc {
+        @Test
+        @DisplayName("Nullを含まない場合")
+        fun testWithoutNull() {
+            val src1 = "arg1" to 1
+            val src2 = Src2("value")
+            val src3 = mapOf("arg3" to 5.5)
+
+            val dsts = mappers.map { it.map(src1, src2, src3) }
+
+            assertEquals(dsts.distinct().size, 1)
+            dsts.first().let {
+                assertEquals(it.arg1, 1)
+                assertEquals(it.arg2, "value")
+                assertEquals(it.arg3, 5.5)
+            }
+        }
+
+        @Test
+        @DisplayName("Nullを含む場合")
+        fun testContainsNull() {
+            val src1 = "arg1" to 7
+            val src2 = Src2(null)
+            val src3 = mapOf("arg3" to BigInteger.TWO)
+
+            val dsts = mappers.map { it.map(src1, src2, src3) }
+
+            assertEquals(dsts.distinct().size, 1)
+            dsts.first().let {
+                assertEquals(it.arg1, 7)
+                assertEquals(it.arg2, null)
+                assertEquals(it.arg3, BigInteger.TWO)
             }
         }
     }
