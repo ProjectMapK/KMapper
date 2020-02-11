f60bf7b96e9b85667848bcd64934b2765e9f7706
wrongwrong <boranti1995@gmail.com>
2020-02-11 22:50:20 +0900

    一部テストを追加

---
+++ src/test/kotlin/mapk/core/SimpleKMapperTest.kt
@@ -0,0 +1,61 @@
+package mapk.core
+
+import com.wrongwrong.mapk.core.KMapper
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.reflect.full.primaryConstructor
+
+data class Dst(
+    val arg1: Int,
+    val arg2: String?,
+    val arg3: Number
+)
+
+@DisplayName("単純なマッピングのテスト")
+class SimpleKMapperTest {
+    val mappers: Set<KMapper<Dst>> = setOf(KMapper(Dst::class), KMapper(Dst::class.primaryConstructor!!))
+
+    @Nested
+    @DisplayName("Map<String, Any?>からマップ")
+    inner class FromMap {
+        @Test
+        @DisplayName("Nullを含まない場合")
+        fun testWithoutNull() {
+            val srcMap: Map<String, Any?> = mapOf(
+                "arg1" to 2,
+                "arg2" to "value",
+                "arg3" to 1.0
+            )
+
+            val dsts = mappers.map { it.map(srcMap) }
+
+            assertEquals(dsts.distinct().size, 1)
+            dsts.first().let {
+                assertEquals(it.arg1, 2)
+                assertEquals(it.arg2, "value")
+                assertEquals(it.arg3, 1.0)
+            }
+        }
+
+        @Test
+        @DisplayName("Nullを含む場合")
+        fun testContainsNull() {
+            val srcMap: Map<String, Any?> = mapOf(
+                "arg1" to 1,
+                "arg2" to null,
+                "arg3" to 2.0f
+            )
+
+            val dsts = mappers.map { it.map(srcMap) }
+
+            assertEquals(dsts.distinct().size, 1)
+            dsts.first().let {
+                assertEquals(it.arg1, 1)
+                assertEquals(it.arg2, null)
+                assertEquals(it.arg3, 2.0f)
+            }
+        }
+    }
+}
