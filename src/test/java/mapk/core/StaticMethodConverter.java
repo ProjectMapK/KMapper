70af196bd7b0edd51ef8d5a99df300eaa4f12809
wrongwrong <boranti1995@gmail.com>
2020-02-15 14:51:00 +0900

    コンバーターを用いるマッピングのテストを追加

---
+++ src/test/java/mapk/core/StaticMethodConverter.java
@@ -0,0 +1,23 @@
+package mapk.core;
+
+import com.wrongwrong.mapk.annotations.KConverter;
+
+import java.util.Arrays;
+
+public class StaticMethodConverter {
+    private final int[] arg;
+
+    private StaticMethodConverter(int[] arg) {
+        this.arg = arg;
+    }
+
+    public int[] getArg() {
+        return arg;
+    }
+
+    @KConverter
+    private static StaticMethodConverter converter(String csv) {
+        int[] arg = Arrays.stream(csv.split(",")).mapToInt(Integer::valueOf).toArray();
+        return new StaticMethodConverter(arg);
+    }
+}
---
+++ src/test/kotlin/mapk/core/ConverterKMapperTest.kt
@@ -0,0 +1,56 @@
+@file:Suppress("unused")
+
+package mapk.core
+
+import com.wrongwrong.mapk.annotations.KConverter
+import com.wrongwrong.mapk.core.KMapper
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+private data class ConstructorConverterDst(val argument: ConstructorConverter)
+private data class ConstructorConverter @KConverter constructor(val arg: Number)
+
+private data class CompanionConverterDst(val argument: CompanionConverter)
+// NOTE: privateクラスのcompanion objectにアクセスする方法を見つけられなかった
+class CompanionConverter private constructor(val arg: String) {
+    private companion object {
+        @KConverter
+        private fun converter(arg: String): CompanionConverter {
+            return CompanionConverter(arg)
+        }
+    }
+}
+
+private data class StaticMethodConverterDst(val argument: StaticMethodConverter)
+
+@DisplayName("コンバータ有りでのマッピングテスト")
+class ConverterKMapperTest {
+    @Test
+    @DisplayName("コンストラクターでのコンバートテスト")
+    fun constructorConverterTest() {
+        val mapper = KMapper(ConstructorConverterDst::class)
+        val result = mapper.map(mapOf("argument" to 1))
+
+        assertEquals(ConstructorConverter(1), result.argument)
+    }
+
+    @Test
+    @DisplayName("コンパニオンオブジェクトに定義したコンバータでのコンバートテスト")
+    fun companionConverterTest() {
+        val mapper = KMapper(CompanionConverterDst::class)
+        val result = mapper.map(mapOf("argument" to "arg"))
+
+        assertEquals("arg", result.argument.arg)
+    }
+
+    @Test
+    @DisplayName("スタティックメソッドに定義したコンバータでのコンバートテスト")
+    fun staticMethodConverterTest() {
+        val mapper = KMapper(StaticMethodConverterDst::class)
+        val result = mapper.map(mapOf("argument" to "1,2,3"))
+
+        assertTrue(intArrayOf(1, 2, 3) contentEquals result.argument.arg)
+    }
+}
