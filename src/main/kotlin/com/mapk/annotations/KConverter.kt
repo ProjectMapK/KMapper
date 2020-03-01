38507532f10c96ed8094d2d3e8a2caa4156358bb
wrongwrong <boranti1995@gmail.com>
2020-03-01 13:02:48 +0900

    分割した内容の削除

--- src/main/kotlin/com/mapk/annotations/KConverter.kt
+++
@@ -1,6 +0,0 @@
-package com.mapk.annotations
-
-@Target(AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
-annotation class KConverter
--- src/main/kotlin/com/mapk/annotations/KPropertyAlias.kt
+++
@@ -1,5 +0,0 @@
-package com.mapk.annotations
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class KPropertyAlias(val value: String)
