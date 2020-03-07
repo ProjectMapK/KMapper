6610bbd62269df7557585d4fd5c328aa30f2a659
wrongwrong <boranti1995@gmail.com>
2020-03-07 15:36:02 +0900

    KPropertyIgnoreに関するテストを追加

---
+++ src/test/kotlin/com/mapk/kmapper/KPropertyIgnoreTest.kt
@@ -0,0 +1,29 @@
+package com.mapk.kmapper
+
+import com.mapk.annotations.KPropertyIgnore
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class KPropertyIgnoreTest {
+    data class Src1(val arg1: Int, val arg2: String, @KPropertyIgnore val arg3: Short)
+    data class Src2(@KPropertyIgnore val arg2: String, val arg3: Int, val arg4: String)
+
+    data class Dst(val arg1: Int, val arg2: String, val arg3: Int, val arg4: String)
+
+    @Test
+    @DisplayName("フィールドを無視するテスト")
+    fun test() {
+        val src1 = Src1(1, "2-1", 31)
+        val src2 = Src2("2-2", 32, "4")
+
+        val mapper = KMapper(::Dst)
+
+        val dst1 = mapper.map(src1, src2)
+        val dst2 = mapper.map(src2, src1)
+
+        assertTrue(dst1 == dst2)
+        assertEquals(Dst(1, "2-1", 32, "4"), dst1)
+    }
+}
