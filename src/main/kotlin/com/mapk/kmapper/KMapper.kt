87d192ece69e734f677c87ab423606a24b48920e
wrongwrong <boranti1995@gmail.com>
2020-03-11 07:42:42 +0900

    新バケツに合わせて対応

--- src/main/kotlin/com/mapk/kmapper/KMapper.kt
+++ src/main/kotlin/com/mapk/kmapper/KMapper.kt
@@ -57,7 +57,7 @@ class KMapper<T : Any> private constructor(
             parameterMap[alias ?: property.name]?.let {
                 // javaGetterを呼び出す方が高速
                 javaGetter.isAccessible = true
-                argumentBucket.setArgument(it.param, javaGetter.invoke(src)?.let { value -> mapObject(it, value) })
+                argumentBucket.putIfAbsent(it.param, javaGetter.invoke(src)?.let { value -> mapObject(it, value) })
                 // 終了判定
                 if (argumentBucket.isInitialized) return
             }
@@ -68,7 +68,7 @@ class KMapper<T : Any> private constructor(
         src.forEach { (key, value) ->
             parameterMap[key]?.let { param ->
                 // 取得した内容がnullでなければ適切にmapする
-                argumentBucket.setArgument(param.param, value?.let { mapObject(param, it) })
+                argumentBucket.putIfAbsent(param.param, value?.let { mapObject(param, it) })
                 // 終了判定
                 if (argumentBucket.isInitialized) return
             }
@@ -77,7 +77,7 @@ class KMapper<T : Any> private constructor(
 
     private fun bindArguments(argumentBucket: ArgumentBucket, srcPair: Pair<*, *>) {
         parameterMap[srcPair.first.toString()]?.let {
-            argumentBucket.setArgument(it.param, srcPair.second?.let { value -> mapObject(it, value) })
+            argumentBucket.putIfAbsent(it.param, srcPair.second?.let { value -> mapObject(it, value) })
         }
     }
 
