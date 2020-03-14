8f20c7c0d274f262975573c66a9947378c78b198
wrongwrong <boranti1995@gmail.com>
2020-03-14 23:20:19 +0900

    Sharedに移植のため削除

--- src/main/kotlin/com/mapk/kmapper/KMapper.kt
+++ src/main/kotlin/com/mapk/kmapper/KMapper.kt
@@ -1,22 +1,19 @@
 package com.mapk.kmapper
 
-import com.mapk.annotations.KConstructor
 import com.mapk.annotations.KGetterAlias
 import com.mapk.annotations.KGetterIgnore
 import com.mapk.core.ArgumentBucket
 import com.mapk.core.EnumMapper
 import com.mapk.core.KFunctionForCall
 import com.mapk.core.getAliasOrName
+import com.mapk.core.toKConstructor
 import java.lang.reflect.Method
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 import kotlin.reflect.KVisibility
-import kotlin.reflect.full.companionObjectInstance
-import kotlin.reflect.full.functions
 import kotlin.reflect.full.isSuperclassOf
 import kotlin.reflect.full.memberProperties
-import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.javaGetter
 
 class KMapper<T : Any> private constructor(
@@ -28,7 +25,7 @@ class KMapper<T : Any> private constructor(
     )
 
     constructor(clazz: KClass<T>, propertyNameConverter: (String) -> String = { it }) : this(
-        getTarget(clazz), propertyNameConverter
+        clazz.toKConstructor(), propertyNameConverter
     )
 
     private val parameterMap: Map<String, ParameterForMap<*>> = function.parameters
@@ -111,29 +108,6 @@ class KMapper<T : Any> private constructor(
 
         return function.call(bucket)
     }
-}
-
-@Suppress("UNCHECKED_CAST")
-internal fun <T : Any> getTarget(clazz: KClass<T>): KFunctionForCall<T> {
-    val factoryConstructor: List<KFunctionForCall<T>> =
-        clazz.companionObjectInstance?.let { companionObject ->
-            companionObject::class.functions
-                .filter { it.annotations.any { annotation -> annotation is KConstructor } }
-                .map { KFunctionForCall(
-                    it,
-                    companionObject
-                ) as KFunctionForCall<T> }
-        } ?: emptyList()
-
-    val constructors: List<KFunctionForCall<T>> = factoryConstructor + clazz.constructors
-        .filter { it.annotations.any { annotation -> annotation is KConstructor } }
-        .map { KFunctionForCall(it) }
-
-    if (constructors.size == 1) return constructors.single()
-
-    if (constructors.isEmpty()) return KFunctionForCall(clazz.primaryConstructor!!)
-
-    throw IllegalArgumentException("Find multiple target.")
 }
 
 private fun <T : Any, R : Any> mapObject(param: ParameterForMap<R>, value: T): Any? {
