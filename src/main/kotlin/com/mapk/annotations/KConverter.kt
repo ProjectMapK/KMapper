ba8382141ce946d5d6f825ae2a871d09d540cb53
wrongwrong <boranti1995@gmail.com>
2020-03-14 22:09:36 +0900

    sharedから移植

---
+++ src/main/kotlin/com/mapk/annotations/KConverter.kt
@@ -0,0 +1,6 @@
+package com.mapk.annotations
+
+@Target(AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class KConverter
