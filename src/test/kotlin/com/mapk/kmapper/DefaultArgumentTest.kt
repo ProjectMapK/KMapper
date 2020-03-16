438e796b5cc1b7e5a981cc6ba84c6ffba197c229
wrongwrong <boranti1995@gmail.com>
2020-03-17 08:36:48 +0900

    デフォルト値を用いたマッピングテストを追加

---
+++ src/test/kotlin/com/mapk/kmapper/DefaultArgumentTest.kt
@@ -0,0 +1,20 @@
+package com.mapk.kmapper
+
+import com.mapk.annotations.KUseDefaultArgument
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("デフォルト引数を指定するテスト")
+class DefaultArgumentTest {
+    data class Dst(val fooArgument: Int, @param:KUseDefaultArgument val barArgument: String = "default")
+    data class Src(val fooArgument: Int, val barArgument: String)
+
+    @Test
+    fun test() {
+        val src = Src(1, "src")
+
+        val result = KMapper(::Dst).map(src)
+        assertEquals(Dst(1, "default"), result)
+    }
+}
