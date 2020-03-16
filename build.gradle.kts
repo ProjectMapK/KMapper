912f35e66bd26ee5f29af19e70505307d72a28fb
wrongwrong <boranti1995@gmail.com>
2020-03-17 08:24:59 +0900

    sharedのアップデート
    同時に依存の伝搬ができていなかった問題を修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation(kotlin("reflect"))
-    implementation("com.github.ProjectMapK:Shared:0.6")
+    api("com.github.ProjectMapK:Shared:0.7")
 
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.6.0") {
