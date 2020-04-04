6163fd1e15e734efdb8e42989357076d8166fc85
wrongwrong <boranti1995@gmail.com>
2020-04-05 06:51:38 +0900

    KMapperの場合をネスト

--- src/test/kotlin/com/mapk/kmapper/PropertyAliasTest.kt
+++ src/test/kotlin/com/mapk/kmapper/PropertyAliasTest.kt
@@ -4,6 +4,7 @@ import com.mapk.annotations.KGetterAlias
 import com.mapk.annotations.KParameterAlias
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 private data class AliasedDst(
@@ -19,28 +20,32 @@ private data class AliasedSrc(
 
 @DisplayName("エイリアスを貼った場合のテスト")
 class PropertyAliasTest {
-    @Test
-    @DisplayName("パラメータにエイリアスを貼った場合")
-    fun paramAliasTest() {
-        val src = mapOf(
-            "arg1" to 1.0,
-            "arg2" to "2",
-            "arg3" to 3
-        )
+    @Nested
+    @DisplayName("KMapper")
+    inner class KMapperTest {
+        @Test
+        @DisplayName("パラメータにエイリアスを貼った場合")
+        fun paramAliasTest() {
+            val src = mapOf(
+                "arg1" to 1.0,
+                "arg2" to "2",
+                "arg3" to 3
+            )
 
-        val result = KMapper(::AliasedDst).map(src)
+            val result = KMapper(::AliasedDst).map(src)
 
-        assertEquals(1.0, result.arg1)
-        assertEquals(3, result.arg2)
-    }
+            assertEquals(1.0, result.arg1)
+            assertEquals(3, result.arg2)
+        }
 
-    @Test
-    @DisplayName("ゲッターにエイリアスを貼った場合")
-    fun getAliasTest() {
-        val src = AliasedSrc(1.0, 2)
-        val result = KMapper(::AliasedDst).map(src)
+        @Test
+        @DisplayName("ゲッターにエイリアスを貼った場合")
+        fun getAliasTest() {
+            val src = AliasedSrc(1.0, 2)
+            val result = KMapper(::AliasedDst).map(src)
 
-        assertEquals(1.0, result.arg1)
-        assertEquals(2, result.arg2)
+            assertEquals(1.0, result.arg1)
+            assertEquals(2, result.arg2)
+        }
     }
 }
