6af91e6d185d2c97ab943ad6a46d5ad729d3ebe2
wrongwrong <boranti1995@gmail.com>
2020-04-05 14:42:37 +0900

    BoundKMapperでパラメータ名変換のテストを追加

--- src/test/kotlin/com/mapk/kmapper/ParameterNameConverterTest.kt
+++ src/test/kotlin/com/mapk/kmapper/ParameterNameConverterTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 private data class CamelCaseDst(val camelCase: String)
+private data class BoundSrc(val camel_case: String)
 
 @DisplayName("パラメータ名変換のテスト")
 class ParameterNameConverterTest {
@@ -25,6 +26,22 @@ class ParameterNameConverterTest {
             val result = mapper.map(src)
 
             assertEquals(expected, result.camelCase)
+        }
+    }
+
+    @Nested
+    @DisplayName("BoundKMapper")
+    inner class BoundKMapperTest {
+        @Test
+        @DisplayName("スネークケースsrc -> キャメルケースdst")
+        fun test() {
+
+            val mapper = BoundKMapper(::CamelCaseDst, BoundSrc::class) {
+                CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, it)
+            }
+            val result = mapper.map(BoundSrc("snakeCase"))
+
+            assertEquals(CamelCaseDst("snakeCase"), result)
         }
     }
 }
