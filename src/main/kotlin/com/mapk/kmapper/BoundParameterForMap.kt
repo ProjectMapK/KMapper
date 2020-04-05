6cbcfca116bf0184384e9c3f1ac5e900473b4954
wrongwrong <boranti1995@gmail.com>
2020-04-05 15:21:36 +0900

    コンバータのサポートを追加

--- src/main/kotlin/com/mapk/kmapper/BoundParameterForMap.kt
+++ src/main/kotlin/com/mapk/kmapper/BoundParameterForMap.kt
@@ -21,8 +21,18 @@ internal class BoundParameterForMap<S : Any>(val param: KParameter, property: KP
         val paramClazz = param.type.classifier as KClass<*>
         val propertyClazz = property.returnType.classifier as KClass<*>
 
-        // TODO: コンバータへの対応
+        val converter = (convertersFromConstructors(paramClazz) +
+                convertersFromStaticMethods(paramClazz) +
+                convertersFromCompanionObject(paramClazz))
+            .filter { (key, _) -> propertyClazz.isSubclassOf(key) }
+            .let {
+                if (1 < it.size) throw IllegalArgumentException("${param.name} has multiple converter. $it")
+
+                it.singleOrNull()?.second
+            }
+
         map = when {
+            converter != null -> { { converter.call(propertyGetter.invoke(it)) } }
             paramClazz.isSubclassOf(propertyClazz) -> { { propertyGetter.invoke(it) } }
             paramClazz.java.isEnum && propertyClazz == String::class -> { {
                 EnumMapper.getEnum(paramClazz.java, propertyGetter.invoke(it) as String)
