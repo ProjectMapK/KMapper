1ad8fc23391cad19c7ab855ed17a94b41f38752f
wrongwrong <boranti1995@gmail.com>
2020-04-11 22:40:43 +0900

    マップ関数はパラメータの中に有った方が適切だったため修正

--- src/main/kotlin/com/mapk/kmapper/PlainParameterForMap.kt
+++ src/main/kotlin/com/mapk/kmapper/PlainParameterForMap.kt
@@ -1,9 +1,11 @@
 package com.mapk.kmapper
 
+import com.mapk.core.EnumMapper
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.isSuperclassOf
 
 internal class PlainParameterForMap<T : Any> private constructor(val param: KParameter, val clazz: KClass<T>) {
     val javaClazz: Class<T> by lazy {
@@ -12,6 +14,25 @@ internal class PlainParameterForMap<T : Any> private constructor(val param: KPar
     // リストの長さが小さいと期待されるためこの形で実装しているが、理想的にはmap的なものが使いたい
     private val converters: Set<Pair<KClass<*>, KFunction<T>>> by lazy {
         convertersFromConstructors(clazz) + convertersFromStaticMethods(clazz) + convertersFromCompanionObject(clazz)
+    }
+
+    fun <U : Any> mapObject(value: U): Any? {
+        val valueClazz: KClass<*> = value::class
+
+        // パラメータに対してvalueが代入可能（同じもしくは親クラス）であればそのまま用いる
+        if (clazz.isSuperclassOf(valueClazz)) return value
+
+        val converter: KFunction<*>? = getConverter(valueClazz)
+
+        return when {
+            // converterに一致する組み合わせが有れば設定されていればそれを使う
+            converter != null -> converter.call(value)
+            // 要求された値がenumかつ元が文字列ならenum mapperでマップ
+            javaClazz.isEnum && value is String -> EnumMapper.getEnum(javaClazz, value)
+            // 要求されているパラメータがStringならtoStringする
+            clazz == String::class -> value.toString()
+            else -> throw IllegalArgumentException("Can not convert $valueClazz to $clazz")
+        }
     }
 
     // 引数の型がconverterに対して入力可能ならconverterを返す
