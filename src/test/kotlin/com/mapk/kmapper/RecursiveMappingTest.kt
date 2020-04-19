cefbf62eeb603032fcfcf577d72b66b3abe03e66
wrongwrong <boranti1995@gmail.com>
2020-04-19 16:54:44 +0900

    パラメータ名変換をテストするためキャメルケース化

--- src/test/kotlin/com/mapk/kmapper/RecursiveMappingTest.kt
+++ src/test/kotlin/com/mapk/kmapper/RecursiveMappingTest.kt
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test
 
 @DisplayName("再帰的マッピングのテスト")
 class RecursiveMappingTest {
-    private data class InnerSrc(val hoge: Int, val fuga: Short, val piyo: String)
-    private data class InnerDst(val hoge: Int, val piyo: String)
+    private data class InnerSrc(val hogeHoge: Int, val fugaFuga: Short, val piyoPiyo: String)
+    private data class InnerDst(val hogeHoge: Int, val piyoPiyo: String)
 
-    private data class Src(val foo: InnerSrc, val bar: Boolean, val baz: Int)
-    private data class Dst(val foo: InnerDst, val baz: Int)
+    private data class Src(val fooFoo: InnerSrc, val barBar: Boolean, val bazBaz: Int)
+    private data class Dst(val fooFoo: InnerDst, val bazBaz: Int)
 
     companion object {
         private val src: Src = Src(InnerSrc(1, 2, "three"), true, 4)
