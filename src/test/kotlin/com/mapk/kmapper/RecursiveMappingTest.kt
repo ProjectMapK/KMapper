ba49c91051aa036e3f10aa287f15da1c3740862f
wrongwrong <boranti1995@gmail.com>
2020-04-19 17:00:31 +0900

    ネストしてパラメータ名変換を適用する場合のテストを追加

--- src/test/kotlin/com/mapk/kmapper/RecursiveMappingTest.kt
+++ src/test/kotlin/com/mapk/kmapper/RecursiveMappingTest.kt
@@ -1,5 +1,6 @@
 package com.mapk.kmapper
 
+import com.google.common.base.CaseFormat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -8,13 +9,16 @@ import org.junit.jupiter.api.Test
 @DisplayName("再帰的マッピングのテスト")
 class RecursiveMappingTest {
     private data class InnerSrc(val hogeHoge: Int, val fugaFuga: Short, val piyoPiyo: String)
+    private data class InnerSnakeSrc(val hoge_hoge: Int, val fuga_fuga: Short, val piyo_piyo: String)
     private data class InnerDst(val hogeHoge: Int, val piyoPiyo: String)
 
     private data class Src(val fooFoo: InnerSrc, val barBar: Boolean, val bazBaz: Int)
+    private data class SnakeSrc(val foo_foo: InnerSnakeSrc, val bar_bar: Boolean, val baz_baz: Int)
     private data class Dst(val fooFoo: InnerDst, val bazBaz: Int)
 
     companion object {
         private val src: Src = Src(InnerSrc(1, 2, "three"), true, 4)
+        private val snakeSrc: SnakeSrc = SnakeSrc(InnerSnakeSrc(1, 2, "three"), true, 4)
         private val expected: Dst = Dst(InnerDst(1, "three"), 4)
     }
 
@@ -22,8 +26,18 @@ class RecursiveMappingTest {
     @DisplayName("BoundKMapper")
     inner class BoundKMapperTest {
         @Test
+        @DisplayName("シンプルなマッピング")
         fun test() {
             val actual = BoundKMapper(::Dst, Src::class).map(src)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        @DisplayName("スネークケースsrc -> キャメルケースdst")
+        fun snakeToCamel() {
+            val actual = BoundKMapper(::Dst, SnakeSrc::class) {
+                CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, it)
+            }.map(snakeSrc)
             assertEquals(expected, actual)
         }
     }
