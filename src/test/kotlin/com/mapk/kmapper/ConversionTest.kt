5ee465f41b411c48f410bdac32e58aa54e85b03d
wrongwrong <boranti1995@gmail.com>
2020-04-25 18:23:45 +0900

    コンバータを用いた変換処理のテストを追加

--- src/test/kotlin/com/mapk/kmapper/ConversionTest.kt
+++ src/test/kotlin/com/mapk/kmapper/ConversionTest.kt
@@ -2,12 +2,17 @@ package com.mapk.kmapper
 
 import com.mapk.conversion.AbstractKConverter
 import com.mapk.conversion.KConvertBy
-import org.junit.jupiter.api.DisplayName
 import java.lang.IllegalArgumentException
 import java.math.BigDecimal
 import java.math.BigInteger
 import kotlin.reflect.KClass
 import kotlin.reflect.jvm.jvmName
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import org.junit.jupiter.params.provider.ValueSource
 
 @DisplayName("KConvertアノテーションによる変換のテスト")
 class ConversionTest {
@@ -49,5 +54,84 @@ class ConversionTest {
 
         override val srcClass = Number::class
         override fun convert(source: Number?): Number? = source?.let(converter)
+    }
+
+    data class Dst(@ToNumber(BigDecimal::class) val number: BigDecimal)
+    data class NumberSrc(val number: Number)
+    data class StringSrc(val number: String)
+
+    enum class NumberSource(val values: Array<Number>) {
+        Doubles(arrayOf(1.0, -2.0, 3.5)),
+        Floats(arrayOf(4.1f, -5.09f, 6.00001f)),
+        Longs(arrayOf(7090, 800, 911)),
+        Ints(arrayOf(0, 123, 234)),
+        Shorts(arrayOf(365, 416, 511)),
+        Bytes(arrayOf(6, 7, 8))
+    }
+
+    @Nested
+    @DisplayName("KMapper")
+    inner class KMapperTest {
+        @ParameterizedTest
+        @EnumSource(NumberSource::class)
+        @DisplayName("Numberソース")
+        fun fromNumber(numbers: NumberSource) {
+            numbers.values.forEach {
+                val actual = KMapper(::Dst).map(NumberSrc(it))
+                assertEquals(0, BigDecimal.valueOf(it.toDouble()).compareTo(actual.number))
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = ["100", "2.0", "-500"])
+        @DisplayName("Stringソース")
+        fun fromString(str: String) {
+            val actual = KMapper(::Dst).map(StringSrc(str))
+            assertEquals(0, BigDecimal(str).compareTo(actual.number))
+        }
+    }
+
+    @Nested
+    @DisplayName("PlainKMapper")
+    inner class PlainKMapperTest {
+        @ParameterizedTest
+        @EnumSource(NumberSource::class)
+        @DisplayName("Numberソース")
+        fun fromNumber(numbers: NumberSource) {
+            numbers.values.forEach {
+                val actual = PlainKMapper(::Dst).map(NumberSrc(it))
+                assertEquals(0, BigDecimal.valueOf(it.toDouble()).compareTo(actual.number))
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = ["100", "2.0", "-500"])
+        @DisplayName("Stringソース")
+        fun fromString(str: String) {
+            val actual = PlainKMapper(::Dst).map(StringSrc(str))
+            assertEquals(0, BigDecimal(str).compareTo(actual.number))
+        }
+    }
+
+    @Nested
+    @DisplayName("BoundKMapper")
+    inner class BoundKMapperTest {
+        @ParameterizedTest
+        @EnumSource(NumberSource::class)
+        @DisplayName("Numberソース")
+        fun fromNumber(numbers: NumberSource) {
+            numbers.values.forEach {
+                val actual = BoundKMapper(::Dst, NumberSrc::class).map(NumberSrc(it))
+                assertEquals(0, BigDecimal.valueOf(it.toDouble()).compareTo(actual.number))
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = ["100", "2.0", "-500"])
+        @DisplayName("Stringソース")
+        fun fromString(str: String) {
+            val actual = BoundKMapper(::Dst, StringSrc::class).map(StringSrc(str))
+            assertEquals(0, BigDecimal(str).compareTo(actual.number))
+        }
     }
 }
