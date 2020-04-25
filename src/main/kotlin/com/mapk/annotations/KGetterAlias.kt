477d06c5ae37e36a94962cf99e9dbcb26badc773
wrongwrong <boranti1995@gmail.com>
2020-04-25 19:55:08 +0900

    @MustBeDocumentedが抜けていたため修正

--- src/main/kotlin/com/mapk/annotations/KGetterAlias.kt
+++ src/main/kotlin/com/mapk/annotations/KGetterAlias.kt
@@ -2,4 +2,5 @@ package com.mapk.annotations
 
 @Target(AnnotationTarget.PROPERTY_GETTER)
 @Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
 annotation class KGetterAlias(val value: String)
