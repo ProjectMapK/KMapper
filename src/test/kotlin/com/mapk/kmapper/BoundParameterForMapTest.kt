f90d07cc48e788415e1b5c10b502fbe4521742bc
wrongwrong <boranti1995@gmail.com>
2020-05-23 16:08:02 +0900

    ToStringのテストを追加

---
+++ src/test/kotlin/com/mapk/kmapper/BoundParameterForMapTest.kt
@@ -0,0 +1,34 @@
+package com.mapk.kmapper
+
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaGetter
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("BoundKMapperのパラメータテスト")
+class BoundParameterForMapTest {
+    data class IntSrc(val int: Int?)
+
+    @Nested
+    @DisplayName("ToStringのテスト")
+    inner class ToStringTest {
+        private val parameter =
+            BoundParameterForMap.ToString<Any>("", IntSrc::class.memberProperties.single().javaGetter!!)
+
+        @Test
+        @DisplayName("not null")
+        fun isNotNull() {
+            val result = parameter.map(IntSrc(1))
+            assertEquals("1", result)
+        }
+
+        @Test
+        @DisplayName("null")
+        fun isNull() {
+            assertNull(parameter.map(IntSrc(null)))
+        }
+    }
+}
