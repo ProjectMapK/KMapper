98a0a5779e348c8ad0ec89a399941e84bb27a31d
wrongwrong <boranti1995@gmail.com>
2020-06-09 04:35:18 +0900

    parameterNameConverterが要る場合と要らない場合を書き分け

--- src/main/kotlin/com/mapk/kmapper/DummyConstructor.kt
+++ src/main/kotlin/com/mapk/kmapper/DummyConstructor.kt
@@ -7,17 +7,25 @@ import com.mapk.kmapper.KMapper as Normal
 import com.mapk.kmapper.PlainKMapper as Plain
 import kotlin.reflect.KFunction
 
+inline fun <reified S : Any, reified D : Any> BoundKMapper(): Bound<S, D> = Bound(D::class, S::class)
+
 inline fun <reified S : Any, reified D : Any> BoundKMapper(
-    noinline parameterNameConverter: ((String) -> String)? = null
-) = Bound(D::class, S::class, parameterNameConverter)
+    noinline parameterNameConverter: ((String) -> String)
+): Bound<S, D> = Bound(D::class, S::class, parameterNameConverter)
+
+inline fun <reified S : Any, D : Any> BoundKMapper(function: KFunction<D>): Bound<S, D> = Bound(function, S::class)
 
 inline fun <reified S : Any, D : Any> BoundKMapper(
     function: KFunction<D>,
-    noinline parameterNameConverter: ((String) -> String)? = null
-) = Bound(function, S::class, parameterNameConverter)
+    noinline parameterNameConverter: ((String) -> String)
+): Bound<S, D> = Bound(function, S::class, parameterNameConverter)
 
-inline fun <reified T : Any> KMapper(noinline parameterNameConverter: ((String) -> String)? = null) =
+inline fun <reified T : Any> KMapper(): Normal<T> = Normal(T::class)
+
+inline fun <reified T : Any> KMapper(noinline parameterNameConverter: ((String) -> String)): Normal<T> =
     Normal(T::class, parameterNameConverter)
 
-inline fun <reified T : Any> PlainKMapper(noinline parameterNameConverter: ((String) -> String)? = null) =
+inline fun <reified T : Any> PlainKMapper(): Plain<T> = Plain(T::class)
+
+inline fun <reified T : Any> PlainKMapper(noinline parameterNameConverter: ((String) -> String)): Plain<T> =
     Plain(T::class, parameterNameConverter)
